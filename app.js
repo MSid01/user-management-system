87bc7f216f1875bef7e94306d41047e888af6579
Siddhesh <mungekarsiddhesh2001@gmail.com>
2021-10-31 12:41:01 +0530

    commented code removed

--- app.js
+++ app.js
@@ -17,18 +17,8 @@ app.use("/static", express.static(path.join(__dirname, "public")));
 app.engine("hbs", exphbs({ extname: "hbs" }));
 app.set("view engine", "hbs");
 
-//connection pool is a cache of dbs connection which can be reused again nd again
-// const pool = mysql.createPool({
-//   connectionLimit: 100,
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD
-// });
-
 const routes = require("./servers/routes/user");
 app.use('/', routes);
-
 
 app.listen(port, () => {
   console.log(`app listning on port ${port}`);
